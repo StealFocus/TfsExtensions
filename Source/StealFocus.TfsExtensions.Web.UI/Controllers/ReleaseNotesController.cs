bd4aec2e475429a3bf934b9270a032518ea06f69
CallumHibbert <callum.hibbert@gmail.com>
2012-01-20 00:15:52 +0000

    Added rough code to connect to TFS and list Team Project Collections.

--- Source/StealFocus.TfsExtensions.Web.UI/Controllers/ReleaseNotesController.cs
+++ Source/StealFocus.TfsExtensions.Web.UI/Controllers/ReleaseNotesController.cs
@@ -1,11 +1,29 @@
 ﻿namespace StealFocus.TfsExtensions.Web.UI.Controllers
 {
+    using System;
+    using System.Collections.ObjectModel;
     using System.Web.Mvc;
+
+    using Microsoft.TeamFoundation.Client;
+    using Microsoft.TeamFoundation.Framework.Client;
+    using Microsoft.TeamFoundation.Framework.Common;
 
     public class ReleaseNotesController : Controller
     {
         public ActionResult Index()
         {
+            Uri tfsUrl = new Uri("http://tfspoc.acme.com");
+            TfsCredentialsProvider tfsCredentialsProvider = new TfsCredentialsProvider("___", "___", "___");
+            TfsConfigurationServer tfsConfigurationServer = TfsConfigurationServerFactory.GetConfigurationServer(tfsUrl, tfsCredentialsProvider);
+            tfsConfigurationServer.Authenticate();
+            ReadOnlyCollection<CatalogNode> collectionNodes = tfsConfigurationServer.CatalogNode.QueryChildren(new[] { CatalogResourceTypes.ProjectCollection }, false, CatalogQueryOptions.None);
+            foreach (CatalogNode collectionNode in collectionNodes)
+            {
+                Guid collectionId = new Guid(collectionNode.Resource.Properties["InstanceId"]);
+                TfsTeamProjectCollection teamProjectCollection = tfsConfigurationServer.GetTeamProjectCollection(collectionId);
+                System.Diagnostics.Debug.WriteLine("tpc name - " + teamProjectCollection.Name);
+            }
+
             return View();
         }
 
---
+++ Source/StealFocus.TfsExtensions/TfsCredentialsProvider.cs
@@ -0,0 +1,27 @@
+﻿namespace StealFocus.TfsExtensions
+{
+    using System;
+    using System.Net;
+
+    using Microsoft.TeamFoundation.Client;
+
+    public class TfsCredentialsProvider : ICredentialsProvider
+    {
+        private readonly NetworkCredential credentials;
+
+        public TfsCredentialsProvider(string user, string domain, string password)
+        {
+            this.credentials = new NetworkCredential(user, password, domain);
+        }
+
+        public ICredentials GetCredentials(Uri uri, ICredentials failedCredentials)
+        {
+            return this.credentials;
+        }
+
+        public void NotifyCredentialsAuthenticated(Uri uri)
+        {
+            // Do nothing.
+        }
+    }
+}
