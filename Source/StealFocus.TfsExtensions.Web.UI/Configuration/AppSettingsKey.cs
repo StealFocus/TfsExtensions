308a12a52b592c89bc6d8a08bbd9ae6c4e56f46f
unknown <hibbc@UK03199.bfl.local>
2012-11-09 13:25:49 +0000

    Change to keys in appSettings, there was a space in the constant causing a bug but keys completely renamed anyway.

--- Source/StealFocus.TfsExtensions.Web.UI/Configuration/AppSettingsKey.cs
+++ Source/StealFocus.TfsExtensions.Web.UI/Configuration/AppSettingsKey.cs
@@ -2,12 +2,12 @@
 {
     public static class AppSettingsKey
     {
-        public const string TfsUrl = "TfsUrl";
+        public const string TfsUrl = "TfsExtensions-TfsUrl";
 
-        public const string TfsUserName = "TfsUserName ";
+        public const string TfsUserName = "TfsExtensions-UserName";
 
-        public const string TfsUserDomain = "TfsUserDomain";
+        public const string TfsUserDomain = "TfsExtensions-UserDomain";
 
-        public const string TfsUserPassword = "TfsUserPassword";
+        public const string TfsUserPassword = "TfsExtensions-UserPassword";
     }
 }
