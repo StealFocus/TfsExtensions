9fdc3c1613ae88b8d72a20f03cc6b7a7a2bbc832
unknown <hibbc@UK03199.bfl.local>
2012-11-20 14:02:25 +0000

    FxCop fix.

--- Source/StealFocus.TfsExtensions/GlobalSuppressions.cs
+++ Source/StealFocus.TfsExtensions/GlobalSuppressions.cs
@@ -18,3 +18,4 @@
 // You do not need to add suppressions to this file manually.
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "StealFocus.TfsExtensions.Build.Client")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "StealFocus.TfsExtensions.Workflow.Activities")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "StealFocus.TfsExtensions")]
